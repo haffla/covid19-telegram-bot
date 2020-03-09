521f9be623a9d6bcca86aa82c8fa13f8903888cb
Jakob Pupke <jakop@pm.me>
2020-03-09 23:25:06 +0100

    fix

--- covid_stats.rb
+++ covid_stats.rb
@@ -5,9 +5,9 @@ class CovidStats
     HTTParty.get("https://raw.githubusercontent.com/iceweasel1/COVID-19-Germany/master/germany_with_source.csv").then do |resp|
       csv = CSV.parse(resp.body)
       berlin = csv.sum { |row| row[2] == "Berlin" ? 1 : 0 }
-      nk = csv.sum { |row| row[2].match? /Neuköll?n/ ? 1 : 0 }
-      kb = csv.sum { |row| row[2] == "Friedrichshain-Kreuzberg" ? 1 : 0 }
-      pan = csv.sum { |row| row[2] == "Pankow" ? 1 : 0 }
+      nk = csv.sum { |row| row[3].match?(/Neuköll?n/) ? 1 : 0 }
+      kb = csv.sum { |row| row[3] == "Friedrichshain-Kreuzberg" ? 1 : 0 }
+      pan = csv.sum { |row| row[3] == "Pankow" ? 1 : 0 }
       [csv.size, berlin, nk, kb, pan]
     end
   end

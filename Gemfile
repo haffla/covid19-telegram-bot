148c6c2f516321310d58d7e13d0da1b9c47a1e83
Jakob Pupke <jakop@pm.me>
2020-03-13 17:05:11 +0100

    raven

--- Gemfile
+++ Gemfile
@@ -6,12 +6,9 @@ source "https://rubygems.org"
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
-# gem "rails"
-
 gem "telegram-bot-ruby", "~> 0.12.0"
-
 gem "httparty", "~> 0.18.0"
-
 gem "redis", "~> 4.1"
-
 gem "nokogiri", "~> 1.10"
+gem "sentry-raven"
+
--- bot.rb
+++ bot.rb
@@ -1,56 +1,72 @@
 # frozen_string_literal: true
 
 require "telegram/bot"
+require "raven"
 require_relative "./covid_rki_stats"
 require_relative "./md_table"
 
-def to_utf8(c)
-  c.chr Encoding::UTF_8
+Raven.configure do |config|
+  config.dsn = ENV["SENTRY_DSN"]
 end
 
-FACE_WITH_MEDICAL_MASK = to_utf8(0x1F637)
-FACE_WITH_HEAD_BANDAGE = to_utf8(0x1F915)
-FACE_WITH_THERMOMETER = to_utf8(0x1F912)
-FACES_SICK = [FACE_WITH_HEAD_BANDAGE, FACE_WITH_THERMOMETER].freeze
+class Bot
+  attr_reader :redis
 
-redis = Redis.new
+  FACE_WITH_MEDICAL_MASK = to_utf8(0x1F637)
+  FACE_WITH_HEAD_BANDAGE = to_utf8(0x1F915)
+  FACE_WITH_THERMOMETER = to_utf8(0x1F912)
+  FACES_SICK = [FACE_WITH_HEAD_BANDAGE, FACE_WITH_THERMOMETER].freeze
 
-Telegram::Bot::Client.run(ENV["TOKEN"], logger: Logger.new(STDOUT)) do |bot|
-  bot.listen do |message|
-    case message.text
-    when "/start"
-      redis.incr "installed"
-      bot.api.send_message(chat_id: message.chat.id, text: "Moin, #{message.from.first_name}. Go /inf")
-    when "/inf", "/trend"
-      show_trend = message.text == "/trend"
-      redis.incr "called"
-      bot.api.send_message(chat_id: message.chat.id, text: "Mom... #{FACE_WITH_MEDICAL_MASK}")
-      sleep 0.5
-      stats, last_updated = CovidRkiStats.new(redis: redis).fetch
-      bot.api.send_message(chat_id: message.chat.id, text: "*#{last_updated}*", parse_mode: "Markdown")
+  def initialize
+    @redis = Redis.new
+  end
 
-      data = stats.map do |state, inf, inf_inc, dead, dead_inc|
-        [
-          state,
-          inf,
-          ("#{format('%+d', inf_inc)}%" if show_trend),
-          dead,
-          ("#{format('%+d', dead_inc)}%" if show_trend)
-        ].compact
-      end
+  def run!
+    Telegram::Bot::Client.run(ENV["TOKEN"], logger: Logger.new(STDOUT)) do |bot|
+      bot.listen do |message|
+        case message.text
+        when "/start"
+          redis.incr "installed"
+          bot.api.send_message(chat_id: message.chat.id, text: "Moin, #{message.from.first_name}. Go /inf")
+        when "/inf", "/trend"
+          show_trend = message.text == "/trend"
+          redis.incr "called"
+          bot.api.send_message(chat_id: message.chat.id, text: "Mom... #{FACE_WITH_MEDICAL_MASK}")
+          sleep 0.5
+          stats, last_updated = CovidRkiStats.new(redis: redis).fetch
+          bot.api.send_message(chat_id: message.chat.id, text: "*#{last_updated}*", parse_mode: "Markdown")
 
-      text = <<~MD
-        ```
-        #{MdTable.make(data: data)}
-        ```
-      MD
+          data = stats.map do |state, inf, inf_inc, dead, dead_inc|
+            [
+              state,
+              inf,
+              ("#{format('%+d', inf_inc)}%" if show_trend),
+              dead,
+              ("#{format('%+d', dead_inc)}%" if show_trend)
+            ].compact
+          end
 
-      bot.api.send_message(chat_id: message.chat.id, text: text, parse_mode: "Markdown")
+          text = <<~MD
+            ```
+            #{MdTable.make(data: data)}
+            ```
+          MD
 
-      if [true, false].sample
-        sleep 2
-        bot.api.send_message(chat_id: message.chat.id, text: FACES_SICK.sample)
+          bot.api.send_message(chat_id: message.chat.id, text: text, parse_mode: "Markdown")
+
+          if [true, false].sample
+            sleep 2
+            bot.api.send_message(chat_id: message.chat.id, text: FACES_SICK.sample)
+          end
+        end
       end
     end
   end
+
+  def to_utf8(c)
+    c.chr Encoding::UTF_8
+  end
 end
+
+Raven.capture { Bot.new.run! }
+

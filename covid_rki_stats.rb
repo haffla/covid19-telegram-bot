f3a25d43f46e7b7f7869f0fe48d312eef4c05a06
Jakob Pupke <jakop@pm.me>
2020-03-14 12:07:09 +0100

    refactoring

--- covid_rki_stats.rb
+++ covid_rki_stats.rb
@@ -28,7 +28,7 @@ class CovidRkiStats
     last_updated = doc.at('h2:contains("Fallzahlen in Deutschland")').next_element.text
     return last_updated if last_updated_only
 
-    data = doc.css("table tbody tr").map do |tr|
+    today = doc.css("table tbody tr").map do |tr|
       tr.children.first(2).map { |e| e.children.first.text }
     end.map do |k, v|
       infected, dead = v.gsub(".", "").scan(/\d+/).map(&:to_i)
@@ -40,15 +40,18 @@ class CovidRkiStats
       [k, infected, dead || 0]
     end
 
-    with_comparison_to_previous(data, last_updated)
-  end
-
-  def with_comparison_to_previous(today, last_updated)
     last_updated_ts = last_updated.scan(/\d+\.\d+\.\d+/).first
     last_updated_ts = Date.parse(last_updated_ts).to_time
+    # under this key stats from yesterday are saved
     y_key = (last_updated_ts - 3600 * 24).strftime("%y.%m.%d") + "_rki"
-    # yesterday stats
-    y_hist = redis.get(y_key).then do |h|
+    with_comparison_to_previous(today, redis.get(y_key)).then do |result|
+      redis.set(last_updated_ts.strftime("%y.%m.%d") + "_rki", today.to_json)
+      [result, last_updated]
+    end
+  end
+
+  def with_comparison_to_previous(today, yesterday)
+    y_hist = yesterday.then do |h|
       if h.nil?
         today
       else
@@ -56,7 +59,7 @@ class CovidRkiStats
       end
     end.to_h { |state, infected, dead| [state, { infected: infected, dead: dead }] }
 
-    res = today.map do |state, infected, dead|
+    today.map do |state, infected, dead|
       y_infected, y_dead = y_hist[state].values_at(:infected, :dead)
       [
         state,
@@ -65,11 +68,6 @@ class CovidRkiStats
         dead,
         y_dead.zero? ? dead : (((dead - y_dead) / y_dead.to_f) * 100).round(2)
       ]
-    end
-
-    [res, last_updated].then do |res|
-      redis.set(last_updated_ts.strftime("%y.%m.%d") + "_rki", today.to_json)
-      res
     end
   end
 end

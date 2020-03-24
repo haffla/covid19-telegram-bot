d1befb00504a2e46daf2fe02d82afbeee6a5f20e
Jakob Pupke <jakop@pm.me>
2020-03-24 20:48:57 +0100

    zeit api change

--- lib/covid_bot/source/die_zeit.rb
+++ lib/covid_bot/source/die_zeit.rb
@@ -5,17 +5,10 @@ module CovidBot
     class DieZeit < Base
       attr_reader :redis
 
-      MONTHS = {
-        "März" => "March",
-        "Mai" => "May",
-        "Oktober" => "October",
-        "Dezember" => "December"
-      }.freeze
-
       def fetch(last_updated_only: false)
-        resp = HTTParty.get("https://interactive.zeit.de/cronjobs/2020/corona/data.json")
+        resp = HTTParty.get("https://interactive.zeit.de/cronjobs/2020/corona/bundeslaender-current.json")
         json = JSON.parse(resp.body)
-        last_updated = json["changeTimestamp"]
+        last_updated = json["lastUpdate"]
         return last_updated if last_updated_only
 
         states = json["states"].map do |s|
@@ -28,20 +21,15 @@ module CovidBot
           [state, infected, deaths, recovered]
         end
 
-        last_updated_ts = parse_date(last_updated)
+        last_updated = DateTime.parse(last_updated)
         t_inf, t_deaths, t_rec = json["totals"].values_at("count", "dead", "recovered")
         states << ["Ges", t_inf, t_deaths, t_rec]
-        y_key = (last_updated_ts - 3600 * 24).strftime("%d.%m.%y") + "_zeit"
+        y_key = (last_updated - 3600 * 24).strftime("%d.%m.%y") + "_zeit"
         with_comparison_to_previous(states, redis.get(y_key)).then do |res|
-          k = last_updated_ts.strftime("%d.%m.%y") + "_zeit"
+          k = last_updated.strftime("%d.%m.%y") + "_zeit"
           redis.set(k, states.to_json)
-          [res, last_updated]
+          [res, last_updated.strftime("%d.%m.%Y %H:%M")]
         end
-      end
-
-      def parse_date(s)
-        s = s.gsub(Regexp.union(MONTHS.keys), MONTHS)
-        Date.parse(s).to_time
       end
     end
   end

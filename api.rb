c690494a149e38ee3d58285b1db99b3cbf084614
Jakob Pupke <jakop@pm.me>
2020-04-20 18:59:21 +0200

    jhu api

--- api.rb
+++ api.rb
@@ -32,11 +32,15 @@ class Application
     zeit_clients = redis.smembers("zeit_clients").map { users[_1] }.compact.sort_by do |el|
       el["last"].then { _1.nil? ? DateTime.new : DateTime.parse(_1) }
     end.reverse!
+    jhu_clients = redis.smembers("jhu_clients").map { users[_1] }.compact.sort_by do |el|
+      el["last"].then { _1.nil? ? DateTime.new : DateTime.parse(_1) }
+    end.reverse!
 
     {
       clients: {
         rki: rki_clients,
-        zeit: zeit_clients
+        zeit: zeit_clients,
+        jhu: jhu_clients
       },
       called: {
         rki: redis.get("called_rki").to_i,

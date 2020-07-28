267a4f21686591cb26395d34efa6d8cf9df649ea
Jakob Pupke <jakop@pm.me>
2020-07-28 12:57:27 +0200

    remove comparison method

--- lib/covid_bot/source/base.rb
+++ lib/covid_bot/source/base.rb
@@ -22,27 +22,6 @@ module CovidBot
         redis.del source_url
       end
 
-      def with_comparison_to_previous(today, yesterday)
-        y_hist = yesterday.then do |h|
-          if h.nil?
-            today
-          else
-            JSON.parse(h).map { |state, *numbers| [state, *numbers.map(&:to_i)] }
-          end
-        end.to_h { |state, *numbers| [state, numbers] }
-
-        sorted(today).filter_map do |state, *numbers|
-          y_numbers = y_hist[state] || []
-
-          numbers.zip(y_numbers).flat_map do |t, y|
-            [
-              t,
-              y ? t - y : nil
-            ]
-          end.then { |res| [state, *res] }
-        end
-      end
-
       def with_data_cache
         redis.get(source_url).then do |data|
           next JSON.parse(data) if data

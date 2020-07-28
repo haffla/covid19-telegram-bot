9f9198ba649db8cf01542e926982fe0f9229f6b8
Jakob Pupke <jakop@pm.me>
2020-07-28 12:27:02 +0200

    jhu use timelines

--- lib/covid_bot/source/johns_hopkins.rb
+++ lib/covid_bot/source/johns_hopkins.rb
@@ -10,41 +10,46 @@ module CovidBot
           process(json)
         end
 
-        last_updated = Time.parse(last_updated)
-        p_key = (last_updated - 3600 * 24).strftime("%y.%m.%d") + "_ju"
-        with_comparison_to_previous(data, redis.get(p_key)).then do |result|
-          redis.set(time.strftime("%y.%m.%d") + "_ju", data.to_json)
-          [
-            result.then { |r| r.first(26).uniq },
-            last_updated.strftime("%d/%m/%Y %H:%M GMT")
-          ]
-        end
+        [
+          sorted(data).first(26).uniq,
+          last_updated
+        ]
       end
 
       private
 
       def process(json)
         top = json["locations"].group_by { |c| c["country_code"] }.map do |country, c|
+          previous, latest = c.flat_map { |x| x["timelines"]["confirmed"]["timeline"].keys }.uniq.last(2)
+          confirmed = c.sum { |r| r["timelines"]["confirmed"]["timeline"][latest] }
+          deaths = c.sum { |r| r["timelines"]["deaths"]["timeline"][latest] }
+          delta_confirmed = confirmed - c.sum { |r| r["timelines"]["confirmed"]["timeline"][previous] }
+          delta_deaths = deaths - c.sum { |r| r["timelines"]["deaths"]["timeline"][previous] }
+
           [
             country,
-            c.sum { |r| r["latest"]["confirmed"] },
-            c.sum { |r| r["latest"]["deaths"] }
+            confirmed,
+            delta_confirmed,
+            deaths,
+            delta_deaths
           ]
         end
 
         totals = [
           "--",
-          json["latest"]["confirmed"],
-          json["latest"]["deaths"]
+          top.sum { |c| c[1] },
+          top.sum { |c| c[2] },
+          top.sum { |c| c[3] },
+          top.sum { |c| c[4] },
         ]
 
-        last_updated_location = json["locations"].max_by { |l| Time.parse(l["last_updated"]) }
 
+        last_updated_location = json["locations"].max_by { |l| Time.parse(l["last_updated"]) }
         [[totals] + top, last_updated_location["last_updated"]]
       end
 
       def source_url
-        "https://coronavirus-tracker-api.herokuapp.com/v2/locations"
+        "https://coronavirus-tracker-api.herokuapp.com/v2/locations?timelines=1"
       end
     end
   end
